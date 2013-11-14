40eaa8b32e5a7a2269aba4785031c6ee12a82790
Akhilesh <akhilesh@localhost.localdomain>
2013-11-14 07:39:23 -0800

    Change for route correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   post '/payupaisa', :to => "payupaisa#express", :as => :payupaisa_express
-  get '/payupaisa/confirm', :to => "payupaisa#confirm", :as => :confirm_payupaisa
+  post '/payupaisa/confirm', :to => "payupaisa#confirm", :as => :confirm_payupaisa
   get '/payupaisa/cancel', :to => "payupaisa#cancel", :as => :cancel_payupaisa
   get '/payupaisa/notify', :to => "payupaisa#notify", :as => :notify_payupaisa
 
